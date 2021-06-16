f7439f1709bd37d404efb3dfa1647c933ad03173
Eugene Brodsky <fupslot@gmail.com>
2021-06-16 10:56:37 +0300

    simplify schema

--- prisma/migrations/20210604182255_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Roles" (
-    "id" TEXT NOT NULL,
-    "alias" VARCHAR(50) NOT NULL,
-    "name" VARCHAR(50) NOT NULL,
-    "description" VARCHAR(255) NOT NULL,
-
-    PRIMARY KEY ("id")
-);
--- prisma/migrations/20210604183438_scopes/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `scopes` to the `Roles` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Roles" ADD COLUMN     "scopes" VARCHAR(255) NOT NULL;
